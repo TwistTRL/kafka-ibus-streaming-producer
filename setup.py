7001bb3b1448c536cae0e0984d389c4c464bda0f
Lingyu Zhou <zhoulytwin@gmail.com>
2019-08-10 18:53:11 -0400

    bugfix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name='kafka-ibus-streaming-producer',
+  name='kafka-tcp-server-producer',
   version='0.1.0',
   author='Lingyu Zhou',
   author_email='zhoulytwin@gmail.com',
-  scripts=['bin/kafka-ibus-streaming-producer.py'],
-  url='https://github.com/TwistTRL/kafka-ibus-streaming-producer',
+  scripts=['bin/kafka-tcp-server-producer.py'],
+  url='https://github.com/TwistTRL/kafka-tcp-server-producer',
   license='GPL-3.0',
-  description='https://github.com/TwistTRL/kafka-ibus-streaming-producer',
+  description='https://github.com/TwistTRL/kafka-tcp-server-producer',
   install_requires=[
     "docopt >= 0.6.1",
     "kafka-python >= 1.4.6"
